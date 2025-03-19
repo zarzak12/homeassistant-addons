79fadf0fd0683e97e52365f6b509a9c22d78457d
zarzak12 <zarzak12@hotmail.com>
2025-03-19 15:12:55 +0100

    V1.0.0

--- Hello/run.sh
+++ Hello/run.sh
@@ -1,3 +1,9 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/sh
 
-echo "Hello world!"
+# Lire l'URL RTMPS depuis la config
+RTMPS_URL=$(jq --raw-output '.rtmps_url' /data/options.json)
+
+echo "Démarrage du flux RTMPS -> HTTP : $RTMPS_URL"
+
+# Convertir RTMPS en HTTP avec FFmpeg
+ffmpeg -i "$RTMPS_URL" -c:v copy -f mjpeg http://0.0.0.0:8080/feed.mjpeg
