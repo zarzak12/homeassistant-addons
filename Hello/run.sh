6e9a9c2ad1064511f7be9cb36f9ca12522a8e235
zarzak12 <zarzak12@hotmail.com>
2025-03-19 16:00:48 +0100

    V1.0.0

--- Hello/run.sh
+++ Hello/run.sh
@@ -6,6 +6,6 @@ RTMPS_URL=$(jq --raw-output '.rtmps_url' /data/options.json)
 echo "Démarrage du flux RTMPS -> HTTP : $RTMPS_URL"
 
 # Convertir RTMPS en HTTP avec FFmpeg
-ffmpeg -i "$RTMPS_URL" -c:v copy -f mjpeg http://0.0.0.:7070/feed.mjpeg
+ffmpeg -i "$RTMPS_URL" -c:v copy -f mjpeg http://0.0.0.0:7070/feed.mjpeg
 
-echo "HTTP : http://0.0.0.:7070/feed.mjpeg"
+echo "HTTP : http://0.0.0.0:7070/feed.mjpeg"
